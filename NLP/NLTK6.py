06d849bebd855c79641945a27c8ba1e434341186
adithyak0604 <adhi11062004@gmail.com>
2025-04-09 12:28:40 +0530

    Delete NLP directory

--- NLP/NLTK6.py
+++
@@ -1,5 +0,0 @@
-from nltk.sentiment import SentimentIntensityAnalyzer
-sentiment_analyzer=SentimentIntensityAnalyzer()
-sentence="Mumbai Indians has won 5 IPL Trophies."
-score=sentiment_analyzer.polarity_scores(sentence)
-print("Sentiment Score:",score)
--- NLP/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-import nltk
-with open('C:/Users/Adithya K/Documents/VS Code/NLP Lab/data.txt','r') as file:
-    text_data=file.read()
-tokens=nltk.word_tokenize(text_data)
-text=nltk.Text(tokens)
-text.concordance("network", width=80, lines=10)
