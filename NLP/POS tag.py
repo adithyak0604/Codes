574904af5b628f9767d9fc4c96d8545433a4a2e4
Adithya Krishna T K <adhi11062004@gmail.com>
2025-05-18 13:24:56 +0530

    Delete NLP directory

--- NLP/POS tag.py
+++
@@ -1,6 +0,0 @@
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag
-text="NLTK is a powerful library"
-tokens=word_tokenize(text)
-pos_tags=pos_tag(tokens)
-print("POS Tags:",pos_tags)
--- NLP/stemlemma.py
+++
@@ -1,9 +0,0 @@
-from nltk.stem import PorterStemmer, WordNetLemmatizer
-from nltk.corpus import wordnet
-stemmer=PorterStemmer()
-lemma=WordNetLemmatizer()
-word="swimming"
-stemmed_word=stemmer.stem(word)
-lemmatized_word=lemma.lemmatize(word, pos=wordnet.VERB)
-print("Stemmed Word:",stemmed_word)
-print("Lemmatized Word:",lemmatized_word)
