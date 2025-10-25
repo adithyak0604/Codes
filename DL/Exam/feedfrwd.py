f9da8e866f0f573f7e16379ed0e781785f296504
Adithya Krishna T K <adhi11062004@gmail.com>
2025-10-25 12:36:42 +0530

    Create feedfrwd.py

---
+++ DL/Exam/feedfrwd.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+
+(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+x_train = x_train.astype('float32') / 255.0
+x_test = x_test.astype('float32') / 255.0
+x_train = x_train.reshape((x_train.shape[0], -1))
+x_test = x_test.reshape((x_test.shape[0], -1))
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+model=models.Sequential([
+    layers.Dense(512, activation='relu', input_shape=(3072,)),
+    layers.Dense(256, activation='relu'),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+history=model.fit(x_train, y_train, epochs=10, batch_size=128, validation_data=(x_test, y_test), verbose=1)
+test_loss, test_acc=model.evaluate(x_test, y_test, verbose=2)
+print(f"\nTest accuracy: {test_acc *100:.2f}%")
+
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.title("Model Accuracy")
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend()
+plt.show()
